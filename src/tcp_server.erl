da36986476b8fb81ea7e916e9b01b1ccd190ff3e
Martin Penckert <martin.penckert@gmail.com>
2019-11-15 08:51:22 +0100

    Adds testing client for nano tcp server.

--- src/tcp_server.erl
+++ src/tcp_server.erl
@@ -1,6 +1,6 @@
 -module(tcp_server).
 
--export([loop/1, start_nano_server/0]).
+-export([loop/1, nano_client_eval/1, start_nano_server/0]).
 
 start_nano_server() ->
   {ok, Listen} = gen_tcp:listen(3000, [binary, {packet, 4},
@@ -16,10 +16,33 @@ loop(Socket) ->
       io:format("Server received binary = ~p~n", [Bin]),
       Str = binary_to_term(Bin),
       io:format("Server (unpacked)  ~p~n", [Str]),
-      Reply = lib_misc:string2value(Str),
+      Reply = string2value(Str),
       io:format("Server replying = ~p~n", [Reply]),
       gen_tcp:send(Socket, term_to_binary(Reply)),
       loop(Socket);
     {tcp_closed, Socket} ->
       io:format("Server socket closed.~n")
   end.
+
+
+%% Client for testing
+nano_client_eval(Str) ->
+  {ok, Socket} =
+    gen_tcp:connect("localhost", 3000,
+                    [binary, {packet, 4}]),
+  ok = gen_tcp:send(Socket, term_to_binary(Str)),
+  receive
+    {tcp, Socket, Bin} ->
+      io:format("Client received binary = ~p~n", [Bin]),
+      Val = binary_to_term(Bin),
+      io:format("Client result = ~p~n", [Val]),
+      gen_tcp:close(Socket)
+    end.
+
+%% Helper
+string2value(Str) ->
+  {ok, Tokens, _}   = erl_scan:string(Str ++ (".")),
+  {ok, Exprs}       = erl_parse:parse_exprs(Tokens),
+  Bindings          = erl_eval:new_bindings(),
+  {value, Value, _} = erl_eval:exprs(Exprs, Bindings),
+  Value.
